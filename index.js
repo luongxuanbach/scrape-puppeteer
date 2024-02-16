fe5eeff178d4b4d4ad813e58f3a5e86c37f0019f
Bachlx <luongxuanbach21112000@gmail.com>
2024-02-16 23:43:30 +0700

    add code

--- index.js
+++ index.js
@@ -4,37 +4,37 @@
 // const browser = startBrowser
 // scrapeController(browser)
 
-const fs = require('fs');
-const puppeteer = require('puppeteer');
+const fs = require("fs");
+const puppeteer = require("puppeteer");
 
 async function run() {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto('https://www.traversymedia.com');
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://www.traversymedia.com");
 
+  // await page.screenshot({ path: 'example.png', fullPage: true});
+  // await page.pdf({ path: 'example.pdf', format: 'A4'});
 
-    // await page.screenshot({ path: 'example.png', fullPage: true});
-    // await page.pdf({ path: 'example.pdf', format: 'A4'});
+  // const html = await page.content();
+  const courses = await page.$$eval("#cscourses .card", (elements) =>
+    elements.map((e) => ({
+      title: e.querySelector(".card-body h3").innerText,
+      level: e.querySelector(".card-body .level").innerText,
 
+      // description: e.querySelector('.card-body span').innerText,
+      url: e.querySelector(".card-footer a").href,
+      thumbnail: e.querySelector(".cscourse-img").src,
+    }))
+  );
+  console.log(courses);
 
-    // const html = await page.content();
-    const courses = await page.$$eval(('#cscourses .card'), (elements) => elements.map( e => ({
-        title: e.querySelector('.card-body h3').innerText,
-        level: e.querySelector('.card-body .level').innerText,
+  //Save data to JSON file
+  fs.writeFile("courses.json", JSON.stringify(courses), (err) => {
+    if (err) throw err;
+    console.log("File saved!");
+  });
 
-        // description: e.querySelector('.card-body span').innerText,
-        url: e.querySelector('.card-footer a').href,
-        thumbnail: e.querySelector('.cscourse-img').src,
-    })));
-    console.log(courses);
-
-    //Save data to JSON file
-    fs.writeFile('courses.json', JSON.stringify(courses), (err) => {
-        if(err) throw err;
-        console.log('File saved!');
-    });
-
-    await browser.close();
+  await browser.close();
 }
 
 run();
